c248566ced61c1150e0dce3f0cc2ca594569c8ac
Marcelo Bernardes <marcelobernardes76@gmail.com>
2024-08-01 10:09:38 -0300

    Buscador de filmes e séries feito no curso alura poo em Java, consumindo API do imdb.

---
+++ src/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,63 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O Poderoso Chefão", 1972);
+        meuFilme.setDuracaoEmMinutos(175);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(10);
+        meuFilme.avalia(9);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie house = new Serie("House", 2004);
+        house.exibeFichaTecnica();
+        house.setTemporadas(8);
+        house.setEpisodiosPorTemporada(22);
+        house.setMinutosPorEpisodio(42);
+        System.out.println("Tempo para maratonar house: " + house.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme("Avatar", 2009);
+        outroFilme.setDuracaoEmMinutos(162);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(house);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(house);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        //filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista " + listaDeFilmes.size());
+        System.out.println(listaDeFilmes);
+        System.out.println("toString do filme " + listaDeFilmes.get(0).toString());
+
+
+    }
+}
